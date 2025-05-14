5ba88df5000941760d999de2b0307b6333ca5b3d
dbstj0403 <wcn09088@naver.com>
2025-05-14 17:52:26 +0900

    Fix: next/font 제거

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import Image from "next/image";
+import logo from "@/assets/join/logo-without-text.svg";
+
 const JoinPage = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col">
+      <Image src={logo} alt="logo" />
+    </div>
+  );
 };
 
 export default JoinPage;
