70d6c78e10f7d8c45f9d64fc55484bfcbc6efa24
daehyun <kimdeahyeon5445@gmail.com>
2025-05-22 10:27:59 +0900

    Feat: 채팅방 나가기 기능 구현

--- src/app/chat-room/[room-id]/page.tsx
+++ src/app/chat-room/[room-id]/page.tsx
@@ -15,7 +15,7 @@ export default function ChatPage() {
     const roomName = searchParams.get("roomName");
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState<Message[]>([]);
-    const [userId, setUserId] = useState<string | null>(null);
+    const [nickName, setNickName] = useState<string | null>(null);
     const socketRef = useRef<WebSocket | null>(null);
     const scrollRef = useRef<HTMLDivElement | null>(null);
 
@@ -25,17 +25,14 @@ export default function ChatPage() {
 
         const connectWebSocket = async () => {
             await fetch("/api/ws");
-            //TODO url 수정 필요
-            const ws = new WebSocket(
-                `ws://172.16.20.105:3001?roomId=${roomId}`
-            );
+            const ws = new WebSocket(`${process.env.NEXT_PUBLIC_WS_URL}?roomId=${roomId}`);
 
             ws.onopen = () => {};
 
             ws.onmessage = (event) => {
                 const data = JSON.parse(event.data);
                 if (data.type === "id") {
-                    setUserId(data.id);
+                    setNickName(data.id);
                 } else {
                     setMessages((prevMessages) => [
                         ...prevMessages,
@@ -73,10 +70,10 @@ export default function ChatPage() {
 
     const sendMessage = (e: React.FormEvent) => {
         e.preventDefault();
-        if (message && socketRef.current && userId) {
+        if (message && socketRef.current && nickName) {
             const messageData: Message = {
                 roomId: Number(roomId),
-                username: userId,
+                username: nickName,
                 message: message,
                 time: new Date(),
             };
@@ -89,9 +86,26 @@ export default function ChatPage() {
         window.history.back();
     };
 
-    const onOutButtonClick = () => {
-        //TODO 방 나가기 로직 구현 필요
-        console.log("방 나가기");
+    const onOutButtonClick = async () => {
+        try {
+            const res = await fetch(`/api/chat/leave-room?roomId=${roomId}`, {
+                method: "DELETE",
+            });
+
+            if (!res.ok) {
+                alert("방 나가기에 실패했습니다.");
+                return;
+            }
+
+            alert("채팅방을 나왔습니다.");
+            window.history.back();
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                console.error(err.message);
+            } else {
+                console.error("알 수 없는 오류:", err);
+            }
+        }
     };
 
     return (
@@ -110,7 +124,7 @@ export default function ChatPage() {
                     {roomName}
                 </div>
             </div>
-            <Messages messages={messages} username={userId ? userId : ""} />
+            <Messages messages={messages} username={nickName ? nickName : ""} />
             <form onSubmit={sendMessage} className="p-4">
                 <div className="flex">
                     <input
