896e69efe88bee45befcb9ca838192e3acd1451d
KimGiii <gibo9809@gmail.com>
2025-05-22 19:31:20 +0900

    Feat: 코트 상세페이지 구현

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                //  http 로만 온다면 "http" 로 바꿔도 됨
+                hostname: "yeyak.seoul.go.kr",
+                port: "",           // 80·443 아닌 경우 "3000" 등 지정
+                pathname: "/**",    // 모든 경로 허용
+            },
+        ],
+    },
+};
 
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+module.exports = nextConfig;
